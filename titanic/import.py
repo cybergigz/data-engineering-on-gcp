966be1d921daeea312b59a2ebb5936eb452cf73e
cybergigz <cybergigz@gmail.com>
2024-02-25 03:50:28 +0000

    create and replace table with pandas

--- titanic/import.py
+++ titanic/import.py
@@ -17,24 +17,8 @@ host = parser.get("mysql_config", "host")
 port = parser.get("mysql_config", "port")
 
 uri = f"mysql+pymysql://{user}:{password}@{host}/{database}"
-engine = sqlalchemy.create_engine(uri)
 
-sql = """
-        CREATE TABLE IF NOT EXISTS titanic (
-            passenger_id           INT,
-            survived               INT,
-            pclass                 INT,
-            name                   VARCHAR(300)
-        )
-    """
-    
-# for k, v in tables.items():
-with engine.connect() as conn:
-     conn.execute(text(f"DROP TABLE IF EXISTS Titanic"))
-     conn.execute(text(sql))
-     print("Created Titanic table successfully")
-
-     df = pd.read_csv("titanic.csv")
-     df.to_sql("titanic", con=uri, if_exists="replace", index=False)
-     print(f"Imported titanic data successfully")
+df = pd.read_csv("titanic.csv")
+df.to_sql("titanic", con=uri, if_exists="replace", index=False)
+print(f"Imported titanic data successfully")
     
