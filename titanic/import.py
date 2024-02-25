6f2e93d64c1cd27e81511fc22e31870eeefcc408
cybergigz <cybergigz@gmail.com>
2024-02-25 03:49:03 +0000

    create table with sqlacamy

--- titanic/import.py
+++ titanic/import.py
@@ -1,11 +1,20 @@
 import configparser
 
-# import pandas as pd
+import pandas as pd
 import sqlalchemy
 from sqlalchemy import text
 
 
 CONFIG_FILE = "local.conf"
+
+parser = configparser.ConfigParser()
+parser.read(CONFIG_FILE)
+
+database = parser.get("mysql_config", "database")
+user = parser.get("mysql_config", "username")
+password = parser.get("mysql_config", "password")
+host = parser.get("mysql_config", "host")
+port = parser.get("mysql_config", "port")
 
 uri = f"mysql+pymysql://{user}:{password}@{host}/{database}"
 engine = sqlalchemy.create_engine(uri)
@@ -17,10 +26,15 @@ sql = """
             pclass                 INT,
             name                   VARCHAR(300)
         )
-    ---"""
+    """
     
-for k, v in tables.items():
-    with engine.connect() as conn:
-        conn.execute(text(f"DROP TABLE IF EXISTS {k}"))
-        conn.execute(text(v))
-        print(f"Created {k} table successfully")
+# for k, v in tables.items():
+with engine.connect() as conn:
+     conn.execute(text(f"DROP TABLE IF EXISTS Titanic"))
+     conn.execute(text(sql))
+     print("Created Titanic table successfully")
+
+     df = pd.read_csv("titanic.csv")
+     df.to_sql("titanic", con=uri, if_exists="replace", index=False)
+     print(f"Imported titanic data successfully")
+    
